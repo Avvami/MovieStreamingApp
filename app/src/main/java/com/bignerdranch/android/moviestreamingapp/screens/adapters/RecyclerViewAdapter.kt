fa6492e27ed64b0901fc65afd32d466b84229771
Avvami <dima.valbu.01@gmail.com>
2022-05-22 14:26:45 +0700

    Retrieve data from Realtime Database

--- app/src/main/java/com/bignerdranch/android/moviestreamingapp/screens/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/moviestreamingapp/screens/adapters/RecyclerViewAdapter.kt
@@ -1,4 +1,40 @@
 package com.bignerdranch.android.moviestreamingapp.screens.adapters
 
-class RecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.moviestreamingapp.R
+import com.bignerdranch.android.moviestreamingapp.data.Resource
+
+class RecyclerViewAdapter(private val resourceList : ArrayList<Resource>) : RecyclerView.Adapter<RecyclerViewAdapter.RVViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.resource_item, parent, false)
+
+        return RVViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RVViewHolder, position: Int) {
+
+        val currentItem = resourceList[position]
+
+        holder.resourceName.text = currentItem.name
+        holder.resourceGenre.text = currentItem.genre
+        holder.resourceYear.text = currentItem.year
+    }
+
+    override fun getItemCount(): Int {
+
+        return resourceList.size
+    }
+
+    class RVViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        val resourceName : TextView = itemView.findViewById(R.id.resource_name)
+        val resourceGenre : TextView = itemView.findViewById(R.id.resource_genre)
+        val resourceYear : TextView = itemView.findViewById(R.id.resource_year)
+    }
 }
--- app/src/main/java/com/bignerdranch/android/moviestreamingapp/screens/fragments/MoviesFragment.kt
+++ app/src/main/java/com/bignerdranch/android/moviestreamingapp/screens/fragments/MoviesFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.moviestreamingapp.R
+import com.bignerdranch.android.moviestreamingapp.data.Resource
+import com.bignerdranch.android.moviestreamingapp.screens.adapters.RecyclerViewAdapter
+import com.google.firebase.database.*
 
 class MoviesFragment : Fragment() {
+
+    private lateinit var dbref : DatabaseReference
+    private lateinit var resourceRecyclerView : RecyclerView
+    private lateinit var resourceArrayList : ArrayList<Resource>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +28,37 @@ class MoviesFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_movies, container, false)
 
+        resourceRecyclerView = view.findViewById(R.id.RVLayout)
+        resourceRecyclerView.layoutManager = LinearLayoutManager(context)
+        resourceRecyclerView.setHasFixedSize(true)
+
+        resourceArrayList = arrayListOf<Resource>()
+        getResourceData()
+
         return view
+    }
+
+    private fun getResourceData() {
+
+        dbref = FirebaseDatabase.getInstance().getReference("Movies")
+
+        dbref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (resourceSnapshot in snapshot.children) {
+
+                        val resource = resourceSnapshot.getValue(Resource::class.java)
+                        resourceArrayList.add(resource!!)
+                    }
+                    resourceRecyclerView.adapter = RecyclerViewAdapter(resourceArrayList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 
 }
